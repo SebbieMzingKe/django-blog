ced0446a4dd21f4f44f88d2cbe706de750884833
mainamichael2849@gmail.com <mainamichael2849@gmail.com>
2024-01-24 15:57:04 +0300

    save data in db, returned posts by id

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path("homepage/", views.homepage, name="posts_home"),
     path("", views.list_post, name = "list_post"),
-    path("<int:post_index>", views.post_detail, name = "post_detail"),
+    path("<int:post_id>", views.post_detail, name = "post_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,11 @@
 from rest_framework.request import Request
-# Create your views here.
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from .models import Post
+from .serializers import PostSerializer
+from django.shortcuts import get_object_or_404
 
-
-posts = [
-    {
-        "id": 1,
-        "title":"why is it difficult to learn programming",
-        "content":"this is to reasons as to why it is hard"
-    },
-     {
-        "id": 2,
-        "title":"learn javascript",
-        "content":"this is a course on javascript"
-    },
-     {
-        "id": 1,
-        "title":"why is it difficult to learn programming",
-        "content":"this is to reasons as to why it is hard"
-    },
-]
 
 
 @api_view(http_method_names=["GET", "POST"])
@@ -33,16 +17,41 @@ def homepage(request: Request):
     response = {"message" : "Hello World!"}
     return Response(data = response, status=status.HTTP_200_OK)
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def list_post(request: Request):
-    return Response(data = posts, status=status.HTTP_200_OK)
+    posts = Post.objects.all()
+    
+    if request.method == "POST":
+        data = request.data
+        serializer = PostSerializer(data = data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message":"Post Created", "data":serializer.data
+            }
+            
+            return Response(data = response, status=status.HTTP_201_CREATED)
+        return Response(data = serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            
+    serializer = PostSerializer(instance = posts, many = True)
+    response = {
+        "message":"posts",
+        "data":serializer.data
+    }
+    return Response(data = response, status=status.HTTP_200_OK)
 
 #returning individual posts
 @api_view(http_method_names=["GET"])
-def post_detail(request: Request, post_index: int):
-    post = posts[post_index]
-    if post:
-        return Response(data = post, status=status.HTTP_200_OK)
+def post_detail(request: Request, post_id: int):
+    post =get_object_or_404(Post, pk = post_id)
+    serializer = PostSerializer(instance = post)
     
-    return Response(data = post, status=status.HTTP_404_NOT_FOUND)    
+    response = {
+        "message":"post",
+        "data":serializer.data
+    }
+    
+    
+    return Response(data = response, status=status.HTTP_404_NOT_FOUND)    
 
